639b03386354aa9c85fc4bd7b74c0b3d43f6c7a3
Jaime Fournier <jaimef@linbsd.org>
2013-09-11 00:22:19 -0700

    Fix pkg file

--- zone-matrix-pkg.el
+++ zone-matrix-pkg.el
@@ -1,3 +1,3 @@
 (define-package "zone-matrix" "0.0.1"
-                 "Matrix themed Zone mode"
-                 '((zone-matrix "0.0.1")))
+  "Matrix themed Zone mode"
+  '((zone-matrix "0.0.1")))
